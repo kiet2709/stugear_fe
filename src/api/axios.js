2d35ffed2d9221dad374b5c65169d3196dfbb4ac
KhaiNguyenDuc <duckhailinux@gmail.com>
2023-10-22 22:16:01 +0700

    [SG-49] Update authenticate

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,46 +1,55 @@
 import axios from "axios";
 import useRefreshToken from "../hooks/useRefreshToken";
-import { async } from "q";
+import { useNavigate } from "react-router-dom";
 
 const BASE_URL = "http://localhost:3000/api";
+
 const axiosPrivate = axios.create({
   baseURL: BASE_URL,
 });
-const axioisPublic = axios.create({
-  baseURL: BASE_URL,
-});
+
+let isRefreshing = false;
 
 axiosPrivate.interceptors.request.use(
   (request) => {
     console.log("Request here");
     if (!request.headers["Authorization"]) {
-      request.headers["Authorization"] = `Bearer ${localStorage.getItem("access_token")}`;
+      request.headers["Authorization"] = `Bearer ${localStorage.getItem(
+        "access_token"
+      )}`;
     }
-    console.log(request)
+    console.log(request);
     return request;
   },
   (error) => Promise.reject(error)
 );
+
 axiosPrivate.interceptors.response.use(
   (response) => {
     return response;
   },
   async (error) => {
-    if(error.response.status === 500){
+    const prevRequest = error?.config;
+    if (
+      (error.response.status === 498 || error.response.status === 403) &&
+      !prevRequest?.sent
+    ) {
+      prevRequest.sent = true;
 
-      console.log("Old token")
-      console.log(localStorage.getItem("access_token"))
-      const refresh = await useRefreshToken()
-      console.log(refresh)
-      if(refresh){
-        localStorage.setItem("access_token",refresh)
+      const refresh = await useRefreshToken();
+
+      if (refresh) {
+        localStorage.setItem("access_token", refresh?.access_token);
+        localStorage.setItem("refresh_token", refresh?.refresh_token);
+        return axiosPrivate(prevRequest);
+      } else {
+        console.log("Hết cứu")
       }
-      console.log("New token")
-      console.log(localStorage.getItem("access_token"))
-    }else{
-      return Promise.reject(error)
+    } else {
+      console.log("Hết cứu thật")
     }
+    return Promise.reject(error);
   }
 );
 
-export { axiosPrivate, axioisPublic };
+export { axiosPrivate };
--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -3,13 +3,24 @@ import axios from "axios";
 const AUTH_URL = 'http://127.0.0.1:8000/api/auth';
 
 const useRefreshToken = () => {
+    console.log({
+        user_id : localStorage.getItem("user_id"),
+        refresh_token: localStorage.getItem("refresh_token"),
+        token: localStorage.getItem("access_token")
+    })
     return (
-        axios.post(AUTH_URL + '/refresh', {
-            headers: {
-                'Authorization': `Bearer ${localStorage.getItem("access_token")}`
+        axios.post(AUTH_URL + '/refresh', 
+            {
+                user_id : localStorage.getItem("user_id"),
+                refresh_token: localStorage.getItem("refresh_token"),
+                token: localStorage.getItem("access_token")
             }
+        )
+        .then(response => {
+            
+            console.log(response)
+            return response?.data?.data
         })
-        .then(response => response?.data?.data)
         .catch(error => error?.response)
     );
 
